25b29cdd4662bacf6c52dabe5a1ab6f47caa1000
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-18 01:38:33 -0700

    Added material.ts imports

---
+++ src/ts/material.ts
@@ -0,0 +1,101 @@
+// button
+import "@material/web/button/elevated-button";
+import "@material/web/button/filled-button";
+import "@material/web/button/filled-tonal-button";
+import "@material/web/button/outlined-button";
+import "@material/web/button/text-button";
+
+// checkbox
+import "@material/web/checkbox/checkbox";
+
+// chips
+import "@material/web/chips/assist-chip";
+import "@material/web/chips/filter-chip";
+import "@material/web/chips/input-chip";
+import "@material/web/chips/suggestion-chip";
+
+// dialog
+import "@material/web/dialog/dialog";
+
+// divider
+import "@material/web/divider/divider";
+
+// elevation
+import "@material/web/elevation/elevation";
+
+// fab
+import "@material/web/fab/fab";
+import "@material/web/fab/branded-fab";
+
+// field
+import "@material/web/field/filled-field";
+import "@material/web/field/outlined-field";
+
+// focus
+import "@material/web/focus/md-focus-ring";
+
+// icon
+import "@material/web/icon/icon";
+
+// icon button
+import "@material/web/iconbutton/icon-button";
+import "@material/web/iconbutton/filled-icon-button";
+import "@material/web/iconbutton/filled-tonal-icon-button";
+import "@material/web/iconbutton/outlined-icon-button";
+
+// badge (LABS)
+import "@material/web/labs/badge/badge";
+
+// navigation bar (LABS)
+import "@material/web/labs/navigationbar/navigation-bar";
+
+// navigation drawer (LABS)
+import "@material/web/labs/navigationdrawer/navigation-drawer";
+import "@material/web/labs/navigationdrawer/navigation-drawer-modal";
+
+// navigation tab (LABS)
+import "@material/web/labs/navigationtab/navigation-tab";
+
+// segmented button (LABS)
+import "@material/web/labs/segmentedbutton/outlined-segmented-button";
+
+// segmented button set (LABS)
+import "@material/web/labs/segmentedbuttonset/outlined-segmented-button-set";
+
+// list
+import "@material/web/list/list";
+import "@material/web/list/list-item";
+
+// menu
+import "@material/web/menu/menu";
+import "@material/web/menu/menu-item";
+import "@material/web/menu/sub-menu-item";
+
+// progress
+import "@material/web/progress/circular-progress";
+import "@material/web/progress/linear-progress";
+
+// radio
+import "@material/web/radio/radio";
+
+// ripple
+import "@material/web/ripple/ripple";
+
+// select
+import "@material/web/select/filled-select";
+import "@material/web/select/outlined-select";
+import "@material/web/select/select-option";
+
+// slider
+import "@material/web/slider/slider";
+
+// switch
+import "@material/web/switch/switch";
+
+// tabs
+import "@material/web/tabs/tabs";
+import "@material/web/tabs/tab";
+
+// text field
+import "@material/web/textfield/filled-text-field";
+import "@material/web/textfield/outlined-text-field";
