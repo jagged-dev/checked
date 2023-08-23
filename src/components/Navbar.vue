ecd8fd842368624a498ccc674ba5443df24525fa
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-23 01:15:09 -0700

    Added navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <nav class="fixed flex w-full p-8">
+        <RouterLink :to="{ name: 'home' }">
+            <div class="flex gap-4">
+                <svg class="h-8 w-8 text-blue"><use xlink:href="/assets/images/icons/icons.svg#logo" /></svg>
+                <h1 class="text-2xl font-bold leading-none text-charcoal transition-font dark:text-ice">checked</h1>
+            </div>
+        </RouterLink>
+    </nav>
+</template>
+
+<style scoped></style>
