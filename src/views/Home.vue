bea9fdc16872a97e627cef3fb4ee619fe933a1df
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-25 01:15:24 -0700

    Updated Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,39 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import Screenshot from "@/components/Screenshot.vue";
+import Title from "@/components/Title.vue";
+import Theme from "@/components/icons/Theme.vue";
 </script>
 
 <template>
-    <main class="bg-ice dark:bg-charcoal">
-        <div class="grid h-screen w-full grid-cols-2 gap-16 p-16">
-            <!-- intro -->
+    <div class="bg-ice transition-background duration-500 dark:bg-charcoal">
+        <!-- hero -->
+        <div class="grid h-screen grid-rows-2 gap-16 p-16 xl:grid-cols-2 xl:grid-rows-3">
+            <!-- blank -->
+            <div class="hidden xl:flex"></div>
+            <!-- screenshot -->
+            <div class="-mx-14 -mt-14 xl:row-span-3 xl:m-0 xl:-mb-16 xl:-mr-16">
+                <Screenshot class="xl:rounded-none xl:rounded-tl-3xl xl:border-b-0 xl:border-r-0" bg="bg-summary-light" bgDark="dark:bg-summary-dark" />
+            </div>
+            <!-- title -->
             <div class="flex flex-col gap-8 place-self-center">
-                <!-- title -->
-                <div class="flex gap-4">
-                    <svg class="ml-[-1rem] h-40 w-40 text-blue"><use xlink:href="/assets/images/icons/icons.svg#logo" /></svg>
-                    <h1 class="text-10xl font-bold leading-none text-charcoal dark:text-ice">Checked</h1>
-                </div>
-                <!-- description -->
-                <p class="text-2xl font-bold text-gunmetal dark:text-silver">Split checks easier than ever with Checked.</p>
-                <!-- nav buttons -->
+                <Title desc="Split checks easier than ever with checked.">checked</Title>
                 <div class="flex gap-4">
                     <RouterLink :to="{ name: 'wizard' }">
-                        <md-filled-button><b>Get Started</b></md-filled-button>
+                        <md-filled-button>Get started</md-filled-button>
                     </RouterLink>
-                    <a href="#learnMore">
-                        <md-elevated-button><b>Learn More</b></md-elevated-button>
+                    <a href="#landing">
+                        <md-outlined-button>Learn more</md-outlined-button>
                     </a>
                 </div>
             </div>
-            <!-- screenshot -->
-            <div class="mb-[-4rem] mr-[-4rem]">
-                <div class="h-full w-full rounded-tl-3xl border-l-8 border-t-8 border-gunmetal border-opacity-25 bg-summary-light bg-cover bg-clip-content dark:border-silver dark:border-opacity-25 dark:bg-summary-dark"></div>
+            <!-- theme toggle -->
+            <div class="flex items-end justify-center">
+                <Theme />
             </div>
         </div>
-
-        <!-- todo -->
-        <div class="flex h-screen w-full items-center justify-center" id="learnMore">
-            <h1 class="text-10xl font-bold text-charcoal dark:text-ice">Learn More</h1>
-        </div>
-    </main>
+        <!-- landing -->
+    </div>
 </template>
 
 <style scoped></style>
