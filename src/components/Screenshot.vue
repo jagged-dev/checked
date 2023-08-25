b1db4f08274ffa7a6a33a252094dbe875f83adf7
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-24 23:01:13 -0700

    Added screenshot component

---
+++ src/components/Screenshot.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const props = defineProps({
+    bg: String,
+    bgDark: String,
+});
+
+const screenshot = ref(null);
+
+onMounted(() => {
+    let div: HTMLDivElement = screenshot.value!;
+    div.classList.add(props.bg!, props.bgDark!);
+});
+</script>
+
+<template>
+    <div class="h-full w-full rounded-3xl border-8 border-gunmetal border-opacity-25 bg-cover bg-clip-content transition dark:border-silver dark:border-opacity-25" ref="screenshot"></div>
+</template>
+
+<style scoped></style>
