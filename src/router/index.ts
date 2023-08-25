920b854d6528c6b0da3abb17345e080edcef3056
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-24 19:43:00 -0700

    Updated paths to use @ alias

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,32 +12,32 @@ const router = createRouter({
     {
       path: "/material",
       name: "material",
-      component: () => import("../views/Material.vue"),
+      component: () => import("@/views/Material.vue"),
     },
     {
       path: "/wizard",
       name: "wizard",
-      component: () => import("../views/wizard/Wizard.vue"),
+      component: () => import("@/views/wizard/Wizard.vue"),
       children: [
         {
           path: "amounts",
           name: "amounts",
-          component: () => import("../views/wizard/tabs/Amounts.vue"),
+          component: () => import("@/views/wizard/tabs/Amounts.vue"),
         },
         {
           path: "people",
           name: "people",
-          component: () => import("../views/wizard/tabs/People.vue"),
+          component: () => import("@/views/wizard/tabs/People.vue"),
         },
         {
           path: "food",
           name: "food",
-          component: () => import("../views/wizard/tabs/Food.vue"),
         },
         {
           path: "summary",
           name: "summary",
           component: () => import("../views/wizard/tabs/Summary.vue"),
+          component: () => import("@/views/wizard/tabs/Food.vue"),
         },
       ],
     },
