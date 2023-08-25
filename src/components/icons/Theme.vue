eaa02bb905a4ce9445ae8c899c74afde4256a8d1
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-24 20:35:05 -0700

    Added theme icon component

---
+++ src/components/icons/Theme.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+    <a class="relative rounded-full p-3 text-charcoal transition-font hover:text-blue dark:text-ice dark:hover:text-blue" href="javascript:toggleTheme()">
+        <svg class="h-6 w-6 transition-transform duration-500 ease-out dark:rotate-180"><use xlink:href="/assets/images/icons/icons.svg#theme" /></svg>
+        <md-ripple></md-ripple>
+    </a>
+</template>
+
+<style scoped></style>
