4b7ddd3fb6d46c8757ea25ed440c7c85496fe66f
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-24 21:14:06 -0700

    Added sidebar component

---
+++ src/components/layout/Sidebar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Theme from "@/components/icons/Theme.vue";
+</script>
+
+<template>
+    <div class="fixed flex h-screen flex-col justify-end gap-3 p-6">
+        <Theme />
+    </div>
+</template>
+
+<style scoped></style>
