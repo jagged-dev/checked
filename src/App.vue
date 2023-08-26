d2df66d4647f27159000378af5a92ea09df4e913
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-26 02:49:56 -0700

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import router from "@/router";
+import Navbar from "@/components/layout/Navbar.vue";
+import Sidebar from "@/components/layout/Sidebar.vue";
+import Footer from "@/components/layout/Footer.vue";
 </script>
 
 <template>
-    <RouterView />
+    <main class="bg-ice transition-background duration-500 dark:bg-charcoal">
+        <RouterView v-if="router.currentRoute.value.name === 'home'" />
+        <div v-else>
+            <Navbar />
+            <Sidebar />
+            <div class="h-screen pl-24 pt-24">
+                <div class="flex h-full flex-col overflow-y-auto rounded-tl-3xl bg-snow transition-background duration-500 dark:bg-coal">
+                    <RouterView />
+                    <Footer></Footer>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped></style>
