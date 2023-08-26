02ec3fac068d0dc3b8a974883301e135ee48fef4
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-26 01:26:02 -0700

    Updated Results.vue

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
-<template>RESULTS</template>
+<template>
+    <div class="flex flex-col gap-8 p-8">
+        <h1 class="text-3xl font-bold leading-none text-charcoal transition-font dark:text-ice">Results</h1>
+    </div>
+</template>
 
 <style scoped></style>
