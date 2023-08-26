6f09e008dc66d5bd97c623bd5947e2b468f988f4
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-26 01:55:25 -0700

    Updated Wizard.vue

--- src/views/wizard/Wizard.vue
+++ src/views/wizard/Wizard.vue
@@ -2,8 +2,6 @@
 import { ref, onMounted, onUpdated } from "vue";
 import { RouterView } from "vue-router";
 import router from "@/router";
-import Navbar from "@/components/layout/Navbar.vue";
-import Sidebar from "@/components/layout/Sidebar.vue";
 
 const wizard = ref(null);
 const activeTab = ref();
@@ -42,32 +40,23 @@ onUpdated(() => {
 </script>
 
 <template>
-    <div class="bg-ice transition-background duration-500 dark:bg-charcoal">
-        <Navbar />
-        <Sidebar />
-
-        <div class="h-screen pl-24 pt-24">
-            <div class="h-full overflow-y-auto rounded-tl-3xl bg-snow transition-background duration-500 dark:bg-coal">
-                <div class="flex flex-col gap-8 p-8">
-                    <!-- active tab -->
-                    <div class="hidden">{{ activeTab }}</div>
-                    <!-- tabs -->
-                    <md-tabs class="rounded-full bg-ice transition-background dark:bg-charcoal" ref="wizard">
-                        <md-tab class="data-selected:bg-blue h-20 rounded-full transition-background">
-                            <div class="flex items-center gap-2"><md-icon slot="icon">payments</md-icon>Amounts</div>
-                        </md-tab>
-                        <md-tab class="data-selected:bg-blue h-20 rounded-full transition-background">
-                            <div class="flex items-center gap-2"><md-icon slot="icon">person</md-icon>People</div>
-                        </md-tab>
-                        <md-tab class="data-selected:bg-blue h-20 rounded-full transition-background">
-                            <div class="flex items-center gap-2"><md-icon slot="icon">restaurant</md-icon>Food</div>
-                        </md-tab>
-                    </md-tabs>
-                    <!-- tab content -->
-                    <RouterView :tabIdx="activeTab" @switchTab="(tabIdx: Number) => (activeTab = tabIdx)" />
-                </div>
-            </div>
-        </div>
+    <div class="flex flex-col gap-8 p-8">
+        <!-- active tab -->
+        <div class="hidden">{{ activeTab }}</div>
+        <!-- tabs -->
+        <md-tabs class="rounded-full bg-ice transition-background dark:bg-charcoal" ref="wizard">
+            <md-tab class="h-20 rounded-full transition-background data-selected:bg-blue">
+                <div class="flex items-center gap-2"><md-icon>payments</md-icon>Amounts</div>
+            </md-tab>
+            <md-tab class="h-20 rounded-full transition-background data-selected:bg-blue">
+                <div class="flex items-center gap-2"><md-icon>person</md-icon>People</div>
+            </md-tab>
+            <md-tab class="h-20 rounded-full transition-background data-selected:bg-blue">
+                <div class="flex items-center gap-2"><md-icon>restaurant</md-icon>Food</div>
+            </md-tab>
+        </md-tabs>
+        <!-- tab content -->
+        <RouterView :tabIdx="activeTab" @switchTab="(tabIdx: Number) => (activeTab = tabIdx)" />
     </div>
 </template>
 
