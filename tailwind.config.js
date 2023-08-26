02f72317ab6fbc578550dbdb5f2ff5986dfac40c
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-25 23:48:30 -0700

    Removed unneeded tailwind mappings

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,26 +72,6 @@ module.exports = {
         background: "background-image, background-color",
         font: "font-size, color",
       },
-      animation: {
-        "fade-200": "fade 0.2s ease-in-out 1",
-        "fade-1000": "fade 1s ease-in-out 1",
-        "slide-up": "slide-up 0.5s ease-out 1",
-        "slide-down": "slide-down 0.5s ease-out 1",
-      },
-      keyframes: {
-        fade: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
-        "slide-up": {
-          "0%": { transform: "translateY(100%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-        "slide-down": {
-          "0%": { transform: "translateY(-100%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-      },
     },
   },
   darkMode: "class",
