edd89b58b2c4cf673ff9e2c3edc5c46a3f042a3b
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-26 02:33:00 -0700

    Updated Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,34 +6,32 @@ import Theme from "@/components/icons/Theme.vue";
 </script>
 
 <template>
-    <div class="bg-ice transition-background duration-500 dark:bg-charcoal">
-        <!-- hero -->
-        <div class="grid h-screen grid-rows-2 gap-16 p-16 xl:grid-cols-2 xl:grid-rows-3">
-            <!-- blank -->
-            <div class="hidden xl:flex"></div>
-            <!-- screenshot -->
-            <div class="-mx-14 -mt-14 xl:row-span-3 xl:m-0 xl:-mb-16 xl:-mr-16">
-                <Screenshot class="xl:rounded-none xl:rounded-tl-3xl xl:border-b-0 xl:border-r-0" bg="bg-results-light" bgDark="dark:bg-results-dark" />
-            </div>
-            <!-- title -->
-            <div class="flex flex-col gap-8 place-self-center">
-                <Title desc="Split checks easier than ever with checked.">checked</Title>
-                <div class="flex gap-4">
-                    <RouterLink :to="{ name: 'wizard' }">
-                        <md-filled-button>Get started</md-filled-button>
-                    </RouterLink>
-                    <a href="#landing">
-                        <md-outlined-button>Learn more</md-outlined-button>
-                    </a>
-                </div>
-            </div>
-            <!-- theme toggle -->
-            <div class="flex items-end justify-center">
-                <Theme />
+    <!-- hero -->
+    <div class="grid h-screen grid-rows-2 gap-16 p-16 xl:grid-cols-2 xl:grid-rows-3">
+        <!-- blank -->
+        <div class="hidden xl:flex"></div>
+        <!-- screenshot -->
+        <div class="-mx-14 -mt-14 xl:row-span-3 xl:m-0 xl:-mb-16 xl:-mr-16">
+            <Screenshot class="xl:rounded-none xl:rounded-tl-3xl xl:border-b-0 xl:border-r-0" bg="bg-results-light" bgDark="dark:bg-results-dark" />
+        </div>
+        <!-- title -->
+        <div class="flex flex-col gap-8 place-self-center">
+            <Title desc="Split checks easier than ever with checked.">checked</Title>
+            <div class="flex gap-4">
+                <RouterLink :to="{ name: 'wizard' }">
+                    <md-filled-button>Get started</md-filled-button>
+                </RouterLink>
+                <a href="#landing">
+                    <md-outlined-button>Learn more</md-outlined-button>
+                </a>
             </div>
         </div>
-        <!-- landing -->
+        <!-- theme toggle -->
+        <div class="flex items-end justify-center">
+            <Theme />
+        </div>
     </div>
+    <!-- landing -->
 </template>
 
 <style scoped></style>
