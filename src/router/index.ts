785f8617462d414418054ac89fccc58b1d5f5192
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-29 21:27:10 -0700

    Added Learn More page

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: Home,
     },
     {
+      path: "/learn",
+      name: "learn",
+      component: () => import("@/views/Learn.vue"),
+    },
+    {
       path: "/material",
       name: "material",
       component: () => import("@/views/Material.vue"),
