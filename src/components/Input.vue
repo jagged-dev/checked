c5d9f96780056f1f72b24afdd8aecbd4ca190639
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-30 23:23:39 -0700

    Added input component

---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+    type: String,
+    label: String,
+    icon: String,
+    prefix: String,
+    value: String,
+});
+
+const emit = defineEmits(["update:value"]);
+</script>
+
+<template>
+    <md-filled-text-field :type="type" :label="label" :prefix-text="prefix" :value="value" @input="(event: any) => $emit('update:value', event.target.value)">
+        <md-icon slot="leadingicon">{{ icon }}</md-icon>
+    </md-filled-text-field>
+</template>
+
+<style scoped></style>
