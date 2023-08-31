4025c0cd314c162f3f2cf2aadaf45f9d4230b9df
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-31 02:06:49 -0700

    Updated Wizard.vue

--- src/views/wizard/Wizard.vue
+++ src/views/wizard/Wizard.vue
@@ -3,39 +3,26 @@ import { ref, onMounted, onUpdated } from "vue";
 import { RouterView } from "vue-router";
 import router from "@/router";
 
-const wizard = ref(null);
+const wizard = ref();
 const activeTab = ref();
-let tabs: any;
+const routes = ["amounts", "party", "food"];
+
+const data = ref({
+    amounts: Object,
+    party: Object,
+    food: Object,
+});
 
 onMounted(() => {
-    tabs = wizard.value!;
+    let tabs: any = wizard.value!;
     activeTab.value = tabs.selected;
-
-    tabs.addEventListener("change", (event: Event) => {
-        activeTab.value = tabs.selected;
-    });
+    tabs.addEventListener("change", () => (activeTab.value = tabs.selected));
 });
 
 onUpdated(() => {
+    let tabs: any = wizard.value!;
     tabs.selected = activeTab.value;
-
-    for (const [i, tab] of Array.from(tabs.children).entries()) {
-        (<any>tab).setAttribute("data-selected", i === activeTab.value ? "true" : "false");
-    }
-
-    switch (activeTab.value) {
-        case 0:
-            router.push({ name: "amounts" });
-            break;
-        case 1:
-            router.push({ name: "party" });
-            break;
-        case 2:
-            router.push({ name: "food" });
-            break;
-        default:
-            break;
-    }
+    router.push({ name: routes[activeTab.value] });
 });
 </script>
 
@@ -45,18 +32,18 @@ onUpdated(() => {
         <div class="hidden">{{ activeTab }}</div>
         <!-- tabs -->
         <md-tabs class="rounded-full bg-ice transition-background dark:bg-charcoal" ref="wizard">
-            <md-tab class="h-20 rounded-full transition-background data-selected:bg-blue">
+            <md-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 0 }">
                 <div class="flex items-center gap-2"><md-icon>payments</md-icon>Amounts</div>
             </md-tab>
-            <md-tab class="h-20 rounded-full transition-background data-selected:bg-blue">
+            <md-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 1 }">
                 <div class="flex items-center gap-2"><md-icon>group</md-icon>Party</div>
             </md-tab>
-            <md-tab class="h-20 rounded-full transition-background data-selected:bg-blue">
+            <md-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 2 }">
                 <div class="flex items-center gap-2"><md-icon>restaurant_menu</md-icon>Food</div>
             </md-tab>
         </md-tabs>
         <!-- tab content -->
-        <RouterView :tabIdx="activeTab" @switchTab="(tabIdx: Number) => (activeTab = tabIdx)" v-slot="{ Component }">
+        <RouterView :tabIdx="activeTab" @switch:tab="(tabIdx: Number) => (activeTab = tabIdx)" @update:amounts="(amounts: any) => (data.amounts = amounts)" @update:party="(party: any) => (data.party = party)" @update:food="(food: any) => (data.food = food)" v-slot="{ Component }">
             <KeepAlive>
                 <component :is="Component" />
             </KeepAlive>
