e781ce2921c40d36985bee6475694818b86a071d
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-31 17:09:52 -0700

    Updated routing to Results.vue

--- src/views/wizard/tabs/Food.vue
+++ src/views/wizard/tabs/Food.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
 import Back from "@/components/Back.vue";
 import Next from "@/components/Next.vue";
 
@@ -10,9 +9,7 @@ const emit = defineEmits(["update:food", "switch:tab"]);
 <template>
     <div class="grid gap-4 lg:grid-cols-2">
         <Back label="Previous" @click="$emit('switch:tab', tabIdx! - 1)">Party</Back>
-        <RouterLink :to="{ name: 'results' }">
-            <Next label="Submit">Results</Next>
-        </RouterLink>
+        <Next label="Submit" @click="$emit('switch:tab', tabIdx! + 1)">Results</Next>
     </div>
 </template>
 
