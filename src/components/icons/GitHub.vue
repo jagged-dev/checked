ce489a5df09f0b37b0c7dd3009d94646a5851843
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-12 01:42:35 -0700

    Removed hyperlinks from profile icons

--- src/components/icons/GitHub.vue
+++ src/components/icons/GitHub.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <a class="relative rounded-full p-3 text-gunmetal transition-font hover:text-blue dark:text-silver dark:hover:text-blue" href="https://www.github.com/jagged-dev/" target="_blank">
-        <svg class="h-6 w-6"><use xlink:href="/assets/images/icons/icons.svg#github" /></svg>
-        <md-ripple></md-ripple>
-    </a>
+    <svg class="h-6 w-6"><use xlink:href="/assets/images/icons/icons.svg#github" /></svg>
 </template>
 
 <style scoped></style>
--- src/components/icons/LinkedIn.vue
+++ src/components/icons/LinkedIn.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <a class="relative rounded-full p-3 text-gunmetal transition-font hover:text-blue dark:text-silver dark:hover:text-blue" href="https://www.linkedin.com/in/jonathan-giang/" target="_blank">
-        <svg class="h-6 w-6"><use xlink:href="/assets/images/icons/icons.svg#linkedin" /></svg>
-        <md-ripple></md-ripple>
-    </a>
+    <svg class="h-6 w-6"><use xlink:href="/assets/images/icons/icons.svg#linkedin" /></svg>
 </template>
 
 <style scoped></style>
