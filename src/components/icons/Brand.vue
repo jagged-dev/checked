3d2324538d073518e6003e1ec752923c636d931f
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-12 19:39:56 -0700

    Updated Brand.vue

--- src/components/icons/Brand.vue
+++ src/components/icons/Brand.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <svg class="text-red"><use xlink:href="/assets/images/icons/icons.svg#brand" /></svg>
+    <svg class="h-6 w-6"><use xlink:href="/assets/images/icons/icons.svg#brand" /></svg>
 </template>
 
 <style scoped></style>
