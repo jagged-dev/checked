0cdbdf735324aae4460b10353fd5af697d83edb6
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-20 16:48:50 -0700

    Added Event.vue

---
+++ src/views/wizard/tabs/Event.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onUpdated } from "vue";
+import Input from "@/components/Input.vue";
+
+const emit = defineEmits(["update:event"]);
+
+const event = ref({
+    name: "",
+});
+
+onUpdated(() => {
+    emit("update:event", event.value);
+});
+</script>
+
+<template>
+    <!-- event -->
+    <div class="flex flex-col gap-4 rounded-3xl bg-ice p-12 transition-background dark:bg-charcoal">
+        <!-- heading -->
+        <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ event.name || "New event" }}</h1>
+        <!-- name -->
+        <Input type="text" label="Event" icon="edit_calendar" :value="event.name" @update:value="(value: string) => (event.name = value)" />
+    </div>
+</template>
+
+<style scoped></style>
