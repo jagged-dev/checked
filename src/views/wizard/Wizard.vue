8db8d97589135d24eb40e5e40e54acc2c647553a
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-20 16:45:21 -0700

    Updated Wizard.vue with event tab and nav buttons

--- src/views/wizard/Wizard.vue
+++ src/views/wizard/Wizard.vue
@@ -2,49 +2,90 @@
 import { ref, onMounted, onUpdated } from "vue";
 import { RouterView } from "vue-router";
 import router from "@/router";
+import Back from "@/components/Back.vue";
+import Next from "@/components/Next.vue";
 
-const routes = ["amounts", "party", "food", "results"];
-const wizard = ref();
+const routes = ["event", "party", "amounts", "food", "results"];
+const tabs = ref();
 const activeTab = ref();
-const amounts = ref();
+const event = ref();
 const party = ref();
+const amounts = ref();
 const food = ref();
 
 onMounted(() => {
-    let tabs: any = wizard.value!;
     activeTab.value = 0;
-    tabs.addEventListener("change", () => (activeTab.value = tabs.activeTabIndex));
+    tabs.value.addEventListener("change", () => (activeTab.value = tabs.value.activeTabIndex));
 });
 
 onUpdated(() => {
-    let tabs: any = wizard.value!;
-    tabs.activeTabIndex = activeTab.value;
+    tabs.value.activeTabIndex = activeTab.value;
     router.push({ name: routes[activeTab.value] });
 });
 </script>
 
 <template>
+    <!-- wizard -->
     <div class="flex flex-col gap-8 p-8">
         <!-- active tab -->
         <div class="hidden">{{ activeTab }}</div>
         <!-- tabs -->
-        <md-tabs class="rounded-full bg-ice transition-background dark:bg-charcoal" :class="{ hidden: activeTab === 3 }" ref="wizard">
+        <md-tabs class="rounded-full bg-ice transition-background dark:bg-charcoal" :class="{ hidden: activeTab === 4 }" ref="tabs">
+            <!-- event -->
             <md-primary-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 0 }">
-                <div class="flex items-center gap-2"><md-icon>payments</md-icon>Amounts</div>
+                <div class="flex items-center gap-2"><md-icon>event</md-icon>Event</div>
             </md-primary-tab>
+            <!-- party -->
             <md-primary-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 1 }">
                 <div class="flex items-center gap-2"><md-icon>group</md-icon>Party</div>
             </md-primary-tab>
+            <!-- amounts -->
             <md-primary-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 2 }">
+                <div class="flex items-center gap-2"><md-icon>payments</md-icon>Amounts</div>
+            </md-primary-tab>
+            <!-- food -->
+            <md-primary-tab class="h-20 rounded-full transition-background" :class="{ 'bg-blue': activeTab === 3 }">
                 <div class="flex items-center gap-2"><md-icon>restaurant_menu</md-icon>Food</div>
             </md-primary-tab>
         </md-tabs>
         <!-- tab content -->
-        <RouterView :tab="activeTab" @switch:tab="(tab: Number) => (activeTab = tab)" :amounts="amounts" @update:amounts="(a: any) => (amounts = a)" :party="party" @update:party="(p: any) => (party = p)" :food="food" @update:food="(f: any) => (food = f)" v-slot="{ Component }">
+        <RouterView :event="event" @update:event="(e: any) => (event = e)" :party="party" @update:party="(p: any) => (party = p)" :amounts="amounts" @update:amounts="(a: any) => (amounts = a)" :food="food" @update:food="(f: any) => (food = f)" v-slot="{ Component }">
             <KeepAlive exclude="Results">
                 <component :is="Component" />
             </KeepAlive>
         </RouterView>
+        <!-- nav buttons -->
+        <div class="grid gap-4 xl:grid-cols-2">
+            <!-- event -->
+            <template v-if="activeTab === 0">
+                <RouterLink :to="{ name: 'home' }">
+                    <Back label="Back">Home</Back>
+                </RouterLink>
+                <Next label="Next" @click="activeTab++">Party</Next>
+            </template>
+            <!-- party -->
+            <template v-if="activeTab === 1">
+                <Back label="Previous" @click="activeTab--">Event</Back>
+                <Next label="Next" @click="activeTab++">Amounts</Next>
+            </template>
+            <!-- amounts -->
+            <template v-if="activeTab === 2">
+                <Back label="Previous" @click="activeTab--">Party</Back>
+                <Next label="Next" @click="activeTab++">Food</Next>
+            </template>
+            <!-- food -->
+            <template v-if="activeTab === 3">
+                <Back label="Previous" @click="activeTab--">Amounts</Back>
+                <Next label="Submit" @click="activeTab++">Results</Next>
+            </template>
+            <!-- results -->
+            <template v-if="activeTab === 4">
+                <Back label="Previous" @click="activeTab--">Food</Back>
+                <RouterLink :to="{ name: 'home' }">
+                    <Next label="Finish">Home</Next>
+                </RouterLink>
+            </template>
+        </div>
     </div>
 </template>
 
