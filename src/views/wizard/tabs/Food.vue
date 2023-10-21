20c3a88e6caab1669bb66a853fd8484477f20ac0
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-20 17:20:04 -0700

    Updated Food.vue

--- src/views/wizard/tabs/Food.vue
+++ src/views/wizard/tabs/Food.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
 import { ref, onUpdated } from "vue";
 import Input from "@/components/Input.vue";
-import Back from "@/components/Back.vue";
-import Next from "@/components/Next.vue";
 
-const props = defineProps({
-    party: Array<any>,
-    tab: Number,
-});
-
-const emit = defineEmits(["update:food", "switch:tab"]);
+const props = defineProps({ party: Array<any> });
+const emit = defineEmits(["update:food"]);
 
 const food = ref([
     {
@@ -48,18 +42,21 @@ function toggleGuest(event: any, item: any) {
 <template>
     <!-- food -->
     <div class="grid gap-8 xl:grid-cols-2" v-if="food.length > 0">
+        <!-- item -->
         <div class="flex flex-col gap-4 rounded-3xl bg-ice p-12 transition-background dark:bg-charcoal xl:last:odd:col-span-2" v-for="item in food">
-            <!-- item -->
+            <!-- heading -->
             <div class="flex items-center">
                 <h1 class="mr-auto text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ item.name || "Item" }}:&ensp;${{ item.price || 0 }}</h1>
                 <md-outlined-icon-button @click="removeItem(item)"><md-icon>close</md-icon></md-outlined-icon-button>
             </div>
-            <Input type="text" label="Item" icon="fastfood" :value="item.name" @update:value="(value: String) => (item.name = value.toString())" />
-            <Input type="number" label="Price" icon="receipt" prefix="$" :value="item.price.toString()" @update:value="(value: Number) => (item.price = Number(value))" />
+            <!-- name -->
+            <Input type="text" label="Item" icon="fastfood" :value="item.name" @update:value="(value: string) => (item.name = value)" />
+            <!-- price -->
+            <Input type="number" label="Price" icon="receipt" prefix="$" :value="item.price.toString()" @update:value="(value: number) => (item.price = value)" />
             <!-- divider -->
             <md-divider></md-divider>
             <!-- guests -->
-            <div class="flex gap-2">
+            <div class="flex flex-wrap gap-2">
                 <md-filter-chip :label="guest.name" @click="(event: any) => toggleGuest(event, item)" v-for="guest in party">
                     <md-icon slot="icon">person</md-icon>
                 </md-filter-chip>
@@ -68,11 +65,6 @@ function toggleGuest(event: any, item: any) {
     </div>
     <!-- add button -->
     <md-text-button @click="addItem"><md-icon slot="icon">add</md-icon>Add item</md-text-button>
-    <!-- nav buttons -->
-    <div class="grid gap-4 xl:grid-cols-2">
-        <Back label="Previous" @click="$emit('switch:tab', tab! - 1)">Party</Back>
-        <Next label="Submit" @click="$emit('switch:tab', tab! + 1)">Results</Next>
-    </div>
 </template>
 
 <style scoped></style>
