8c85ce93349f4963a3c3e11330ca15fc8de3aaf5
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-25 22:00:56 -0700

    Added Select.vue

---
+++ src/components/Select.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+    modelValue: String,
+    icon: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+    <md-filled-select hasLeadingIcon quick :value="modelValue" @change="$emit('update:modelValue', $event.target.value)">
+        <md-icon slot="leading-icon">{{ icon }}</md-icon>
+        <md-select-option displayText="">
+            <div slot="headline">...</div>
+        </md-select-option>
+        <slot></slot>
+    </md-filled-select>
+</template>
+
+<style scoped></style>
