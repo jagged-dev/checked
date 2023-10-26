2ec95853fc9e1a5bd8d7bbb07f2a34ca28a0f8a0
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-25 21:38:30 -0700

    Updated Results.vue

--- src/views/wizard/tabs/Results.vue
+++ src/views/wizard/tabs/Results.vue
@@ -1,68 +1,29 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onActivated } from "vue";
 import Input from "@/components/Input.vue";
 
 const props = defineProps({
+    event: Object,
+    party: Array<string>,
     amounts: Object,
-    party: Array<any>,
     food: Array<any>,
 });
 
-const results: any = ref({});
+const results = ref();
 
-for (let guest of props.party!) {
-    results.value[guest.name] = {
-        food: [],
-        subtotal: 0,
-        tax: 0,
-        tip: 0,
-        total: 0,
-    };
-}
-
-for (let item of props.food!) {
-    let itemSplit = item.price / item.guests.length;
-    for (let guest of item.guests) {
-        results.value[guest].food.push({
-            name: item.name,
-            price: Number(itemSplit.toFixed(2)),
-        });
-        results.value[guest].subtotal = Number((results.value[guest].subtotal + itemSplit).toFixed(2));
-    }
-}
-
-for (let guest in results.value) {
-    let subtotalSplit = results.value[guest].subtotal / props.amounts!.subtotal;
-    results.value[guest].tax = Number((props.amounts!.tax * subtotalSplit).toFixed(2));
-    results.value[guest].tip = Number((props.amounts!.tip * subtotalSplit).toFixed(2));
-    results.value[guest].total = Number((results.value[guest].subtotal + results.value[guest].tax + results.value[guest].tip).toFixed(2));
-}
+onActivated(() => {});
 </script>
 
 <template>
     <!-- heading -->
-    <h1 class="text-3xl font-bold leading-none text-charcoal transition-font dark:text-ice">Results</h1>
+    <h1 class="text-3xl font-bold leading-none text-charcoal transition-font dark:text-ice">{{ event?.name }}</h1>
     <!-- results -->
-    <div class="flex flex-col gap-4 px-8" v-for="guest in party">
-        <!-- guest -->
-        <div class="flex items-center gap-4">
-            <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ guest.name }}</h1>
-            <md-text-button><md-icon slot="icon">share</md-icon>Share</md-text-button>
-        </div>
-        <!-- divider -->
-        <md-divider></md-divider>
-        <!-- food -->
-        <Input type="text" label="Item" icon="fastfood" :suffix="'$' + item.price" :value="item.name" disabled v-for="item in results[guest.name].food" />
-        <!-- divider -->
-        <md-divider></md-divider>
-        <!-- amounts -->
-        <Input type="text" label="Amount" icon="receipt" :suffix="'$' + results[guest.name].subtotal" value="Subtotal" disabled />
-        <Input type="text" label="Amount" icon="receipt" :suffix="'$' + results[guest.name].tax" value="Tax" disabled />
-        <Input type="text" label="Amount" icon="receipt" :suffix="'$' + results[guest.name].tip" value="Tip" disabled />
-        <!-- divider -->
-        <md-divider></md-divider>
-        <!-- total -->
-        <h1 class="ml-auto text-xl font-bold text-charcoal transition-font dark:text-ice">Total:&ensp;${{ results[guest.name].total }}</h1>
+    <div class="flex flex-col gap-4">
+        <span>{{ event }}</span>
+        <span>{{ party }}</span>
+        <span>{{ amounts }}</span>
+        <span>{{ food }}</span>
+        <span>{{ results }}</span>
     </div>
 </template>
 
