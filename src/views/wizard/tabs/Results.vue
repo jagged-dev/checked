258c4435cb36e1647f900a3a5518ca55f8b184e5
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-10-30 23:25:00 -0700

    Updated Results.vue

--- src/views/wizard/tabs/Results.vue
+++ src/views/wizard/tabs/Results.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, onActivated } from "vue";
-import Input from "@/components/Input.vue";
 
 const props = defineProps({
     event: Object,
@@ -10,21 +9,47 @@ const props = defineProps({
 });
 
 const results = ref();
+const expanded = ref();
 
 onActivated(() => {});
+
+function toggleGuest(guest: string, selected: boolean) {
+    if (!selected) expanded.value = guest;
+    else expanded.value = "";
+}
 </script>
 
 <template>
-    <!-- heading -->
-    <h1 class="text-3xl font-bold leading-none text-charcoal transition-font dark:text-ice">{{ event?.name }}</h1>
     <!-- results -->
-    <div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xs:p-12">
+        <!-- heading -->
+        <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ event?.name }}</h1>
+        <!-- divider -->
+        <md-divider></md-divider>
+        <!-- guests -->
+        <div class="flex flex-wrap gap-2">
+            <md-assist-chip label="View guest" disabled>
+                <md-icon slot="icon">visibility</md-icon>
+            </md-assist-chip>
+            <md-filter-chip :label="guest" :selected="guest === expanded" @click="toggleGuest(guest, $event.target.selected)" v-for="guest in party">
+                <md-icon slot="icon">add</md-icon>
+            </md-filter-chip>
+        </div>
+    </div>
+    <!-- details -->
+    <div class="flex flex-col" :class="{ hidden: guest !== expanded }" v-for="guest in party">
+        <div class="rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal">
+            <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">{{ guest }}</h1>
+        </div>
+    </div>
+    <!-- test -->
+    <!-- <div class="flex flex-col gap-4">
         <span>{{ event }}</span>
         <span>{{ party }}</span>
         <span>{{ amounts }}</span>
         <span>{{ food }}</span>
         <span>{{ results }}</span>
-    </div>
+    </div> -->
 </template>
 
 <style scoped></style>
