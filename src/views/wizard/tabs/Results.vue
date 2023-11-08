779c277d560f3b18991860b8974c0ae89624a2cc
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-11-07 22:51:32 -0800

    Updated Results.vue

--- src/views/wizard/tabs/Results.vue
+++ src/views/wizard/tabs/Results.vue
@@ -8,11 +8,81 @@ const props = defineProps({
     food: Array<any>,
 });
 
-const results = ref();
+const results = ref<any>({});
 const expanded = ref();
 
 onMounted(() => {
     if (props.event && props.party && props.amounts && props.food) {
+        let taxPct = Number(props.amounts.tax) / Number(props.amounts.subtotal);
+        let tipPct = Number(props.amounts.tip) / Number(props.amounts.subtotal);
+
+        for (let guest of props.party!) {
+            results.value[guest] = {
+                items: [],
+                send: {},
+                receive: {},
+            };
+            for (let guest2 of props.party!) {
+                if (guest2 !== guest) {
+                    results.value[guest].send[guest2] = 0;
+                    results.value[guest].receive[guest2] = 0;
+                }
+            }
+            results.value[guest].send.total = 0;
+            results.value[guest].receive.total = 0;
+        }
+
+        for (let item of props.food!) {
+            let itemSubtotal = Number(item.price);
+            let itemTax = itemSubtotal * taxPct;
+            let itemTip = itemSubtotal * tipPct;
+            let itemTotal = itemSubtotal + itemTax + itemTip;
+
+            let splitSubtotal = itemSubtotal / item.guests.length;
+            let splitTax = itemTax / item.guests.length;
+            let splitTip = itemTip / item.guests.length;
+            let splitTotal = itemTotal / item.guests.length;
+
+            for (let guest of item.guests) {
+                results.value[guest].items.push({
+                    name: item.name,
+                    amounts: {
+                        itemSubtotal: itemSubtotal,
+                        itemTax: itemTax,
+                        itemTip: itemTip,
+                        itemTotal: itemTotal,
+                        splitSubtotal: splitSubtotal,
+                        splitTax: splitTax,
+                        splitTip: splitTip,
+                        splitTotal: splitTotal,
+                    },
+                    purchaser: item.purchaser,
+                    guests: item.guests,
+                });
+                if (guest !== item.purchaser) {
+                    let amount = results.value[guest].send[item.purchaser];
+                    results.value[guest].send[item.purchaser] = amount + splitTotal;
+                    results.value[item.purchaser].receive[guest] = amount + splitTotal;
+
+                    let sendTotal = results.value[guest].send.total;
+                    let receiveTotal = results.value[item.purchaser].receive.total;
+                    results.value[guest].send.total = sendTotal + splitTotal;
+                    results.value[item.purchaser].receive.total = receiveTotal + splitTotal;
+                } else {
+                    let sendTotal = results.value[item.purchaser].send.total;
+                    results.value[item.purchaser].send.total = sendTotal + itemTotal;
+                }
+            }
+        }
+
+        for (let guest of props.party!) {
+            for (let guest2 in results.value[guest].send) {
+                results.value[guest].send[guest2] = Number(results.value[guest].send[guest2]).toFixed(2);
+            }
+            for (let guest2 in results.value[guest].receive) {
+                results.value[guest].receive[guest2] = Number(results.value[guest].receive[guest2]).toFixed(2);
+            }
+        }
     }
 });
 
@@ -40,25 +110,53 @@ function toggleGuest(guest: string, selected: boolean) {
         </div>
     </div>
     <!-- details -->
-    <div class="grid gap-4 xl:grid-cols-3" :class="{ hidden: guest !== expanded }" v-for="guest in party">
-        <div class="rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-3">
+    <div class="grid gap-8 xl:grid-cols-3" :class="{ hidden: guest !== expanded }" v-for="guest in party">
+        <!-- overview -->
+        <div class="flex items-center rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-3">
             <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">{{ guest }}</h1>
+            <div class="flex w-full justify-end gap-4">
+                <h1 class="text-xl font-bold text-gunmetal transition-font dark:text-silver">${{ results[guest]?.receive?.total }}</h1>
+                <h1 class="text-xl font-bold text-blue">${{ results[guest]?.send?.total }}</h1>
+            </div>
         </div>
-        <div class="rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal">
-            <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">Guests</h1>
+        <!-- guests -->
+        <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal">
+            <template v-for="g in party">
+                <div class="flex" v-if="g !== guest">
+                    <h1 class="font-bold text-charcoal transition-font dark:text-ice">{{ g }}</h1>
+                    <div class="flex w-full justify-end gap-4">
+                        <h1 class="font-bold text-gunmetal transition-font dark:text-silver">${{ results[guest]?.receive[g] }}</h1>
+                        <h1 class="font-bold text-blue">${{ results[guest]?.send[g] }}</h1>
+                    </div>
+                </div>
+            </template>
         </div>
-        <div class="rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-2">
+        <!-- items -->
+        <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-2">
             <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">Items</h1>
+            <div class="flex flex-col" v-for="item in results[guest]?.items">
+                <span>{{ item.name }}</span>
+                <span>{{ item.amounts.itemSubtotal }}</span>
+                <span>{{ item.amounts.itemTax }}</span>
+                <span>{{ item.amounts.itemTip }}</span>
+                <span>{{ item.amounts.itemTotal }}</span>
+                <span>{{ item.amounts.splitSubtotal }}</span>
+                <span>{{ item.amounts.splitTax }}</span>
+                <span>{{ item.amounts.splitTip }}</span>
+                <span>{{ item.amounts.splitTotal }}</span>
+                <span>{{ item.purchaser }}</span>
+                <span>{{ item.guests }}</span>
+            </div>
         </div>
     </div>
     <!-- test -->
-    <!-- <div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-4">
         <span>{{ event }}</span>
         <span>{{ party }}</span>
         <span>{{ amounts }}</span>
         <span>{{ food }}</span>
         <span>{{ results }}</span>
-    </div> -->
+    </div>
 </template>
 
 <style scoped></style>
