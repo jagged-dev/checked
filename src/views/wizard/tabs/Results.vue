89dbf62f46d670870ee80759b004c6da43fa684f
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-11-13 17:04:39 -0800

    Updated Results.vue

--- src/views/wizard/tabs/Results.vue
+++ src/views/wizard/tabs/Results.vue
@@ -19,17 +19,11 @@ onMounted(() => {
         for (let guest of props.party!) {
             results.value[guest] = {
                 items: [],
-                send: {},
-                receive: {},
+                subtotal: "",
+                tax: "",
+                tip: "",
+                total: "",
             };
-            for (let guest2 of props.party!) {
-                if (guest2 !== guest) {
-                    results.value[guest].send[guest2] = 0;
-                    results.value[guest].receive[guest2] = 0;
-                }
-            }
-            results.value[guest].send.total = 0;
-            results.value[guest].receive.total = 0;
         }
 
         for (let item of props.food!) {
@@ -47,40 +41,21 @@ onMounted(() => {
                 results.value[guest].items.push({
                     name: item.name,
                     amounts: {
-                        itemSubtotal: itemSubtotal,
-                        itemTax: itemTax,
-                        itemTip: itemTip,
-                        itemTotal: itemTotal,
-                        splitSubtotal: splitSubtotal,
-                        splitTax: splitTax,
-                        splitTip: splitTip,
-                        splitTotal: splitTotal,
+                        itemSubtotal: itemSubtotal.toFixed(2),
+                        itemTax: itemTax.toFixed(2),
+                        itemTip: itemTip.toFixed(2),
+                        itemTotal: itemTotal.toFixed(2),
+                        splitSubtotal: splitSubtotal.toFixed(2),
+                        splitTax: splitTax.toFixed(2),
+                        splitTip: splitTip.toFixed(2),
+                        splitTotal: splitTotal.toFixed(2),
                     },
-                    purchaser: item.purchaser,
                     guests: item.guests,
                 });
-                if (guest !== item.purchaser) {
-                    let amount = results.value[guest].send[item.purchaser];
-                    results.value[guest].send[item.purchaser] = amount + splitTotal;
-                    results.value[item.purchaser].receive[guest] = amount + splitTotal;
-
-                    let sendTotal = results.value[guest].send.total;
-                    let receiveTotal = results.value[item.purchaser].receive.total;
-                    results.value[guest].send.total = sendTotal + splitTotal;
-                    results.value[item.purchaser].receive.total = receiveTotal + splitTotal;
-                } else {
-                    let sendTotal = results.value[item.purchaser].send.total;
-                    results.value[item.purchaser].send.total = sendTotal + itemTotal;
-                }
-            }
-        }
-
-        for (let guest of props.party!) {
-            for (let guest2 in results.value[guest].send) {
-                results.value[guest].send[guest2] = Number(results.value[guest].send[guest2]).toFixed(2);
-            }
-            for (let guest2 in results.value[guest].receive) {
-                results.value[guest].receive[guest2] = Number(results.value[guest].receive[guest2]).toFixed(2);
+                results.value[guest].subtotal = (Number(results.value[guest].subtotal) + splitSubtotal).toFixed(2);
+                results.value[guest].tax = (Number(results.value[guest].tax) + splitTax).toFixed(2);
+                results.value[guest].tip = (Number(results.value[guest].tip) + splitTip).toFixed(2);
+                results.value[guest].total = (Number(results.value[guest].total) + splitTotal).toFixed(2);
             }
         }
     }
@@ -101,8 +76,8 @@ function toggleGuest(guest: string, selected: boolean) {
         <md-divider></md-divider>
         <!-- guests -->
         <div class="flex flex-wrap gap-2">
-            <md-assist-chip label="View results" disabled>
-                <md-icon slot="icon">visibility</md-icon>
+            <md-assist-chip label="Results" disabled>
+                <md-icon slot="icon">filter_list</md-icon>
             </md-assist-chip>
             <md-filter-chip :label="guest" :selected="guest === expanded" @click="toggleGuest(guest, $event.target.selected)" v-for="guest in party">
                 <md-icon slot="icon">arrow_drop_down</md-icon>
@@ -111,51 +86,131 @@ function toggleGuest(guest: string, selected: boolean) {
     </div>
     <!-- details -->
     <div class="grid gap-8 xl:grid-cols-3" :class="{ hidden: guest !== expanded }" v-for="guest in party">
-        <!-- overview -->
-        <div class="flex items-center rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-3">
-            <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">{{ guest }}</h1>
-            <div class="flex w-full justify-end gap-4">
-                <h1 class="text-xl font-bold text-gunmetal transition-font dark:text-silver">${{ results[guest]?.receive?.total }}</h1>
-                <h1 class="text-xl font-bold text-blue">${{ results[guest]?.send?.total }}</h1>
-            </div>
-        </div>
-        <!-- guests -->
-        <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal">
-            <template v-for="g in party">
-                <div class="flex" v-if="g !== guest">
-                    <h1 class="font-bold text-charcoal transition-font dark:text-ice">{{ g }}</h1>
-                    <div class="flex w-full justify-end gap-4">
-                        <h1 class="font-bold text-gunmetal transition-font dark:text-silver">${{ results[guest]?.receive[g] }}</h1>
-                        <h1 class="font-bold text-blue">${{ results[guest]?.send[g] }}</h1>
-                    </div>
+        <!-- guest -->
+        <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-3 xl:flex-row">
+            <!-- name -->
+            <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ guest }}</h1>
+            <!-- amounts -->
+            <div class="flex w-full flex-wrap gap-8 gap-y-2 xl:justify-end">
+                <!-- subtotal -->
+                <div class="flex items-center gap-2">
+                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ results[guest]?.subtotal }}</h1>
+                    <p class="text-sm text-gunmetal transition-font dark:text-silver">(Subtotal)</p>
                 </div>
-            </template>
+                <!-- tax -->
+                <div class="flex items-center gap-2">
+                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ results[guest]?.tax }}</h1>
+                    <p class="text-sm text-gunmetal transition-font dark:text-silver">(Tax)</p>
+                </div>
+                <!-- tip -->
+                <div class="flex items-center gap-2">
+                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ results[guest]?.tip }}</h1>
+                    <p class="text-sm text-gunmetal transition-font dark:text-silver">(Tip)</p>
+                </div>
+                <!-- total -->
+                <div class="flex items-center gap-2">
+                    <h1 class="text-xl font-bold text-blue">${{ results[guest]?.total }}</h1>
+                    <p class="text-sm text-gunmetal transition-font dark:text-silver">(Total)</p>
+                </div>
+            </div>
         </div>
         <!-- items -->
-        <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-2">
-            <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">Items</h1>
-            <div class="flex flex-col" v-for="item in results[guest]?.items">
-                <span>{{ item.name }}</span>
-                <span>{{ item.amounts.itemSubtotal }}</span>
-                <span>{{ item.amounts.itemTax }}</span>
-                <span>{{ item.amounts.itemTip }}</span>
-                <span>{{ item.amounts.itemTotal }}</span>
-                <span>{{ item.amounts.splitSubtotal }}</span>
-                <span>{{ item.amounts.splitTax }}</span>
-                <span>{{ item.amounts.splitTip }}</span>
-                <span>{{ item.amounts.splitTotal }}</span>
-                <span>{{ item.purchaser }}</span>
-                <span>{{ item.guests }}</span>
+        <div class="flex flex-col gap-8 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:col-span-3">
+            <!-- item -->
+            <div class="flex flex-col gap-2" v-for="item in results[guest]?.items">
+                <!-- name -->
+                <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ item.name }}</h1>
+                <!-- amounts -->
+                <table>
+                    <tbody>
+                        <!-- subtotal -->
+                        <tr class="border-b border-gunmetal border-opacity-25 transition dark:border-silver dark:border-opacity-25">
+                            <!-- label -->
+                            <td class="w-full px-4 py-2">
+                                <p class="font-bold text-gunmetal transition-font dark:text-silver">Subtotal</p>
+                            </td>
+                            <!-- item subtotal -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.itemSubtotal }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;item</p>
+                                </div>
+                            </td>
+                            <!-- split subtotal -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.splitSubtotal }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;person</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- tax -->
+                        <tr class="border-b border-gunmetal border-opacity-25 transition dark:border-silver dark:border-opacity-25">
+                            <!-- label -->
+                            <td class="w-full px-4 py-2">
+                                <p class="font-bold text-gunmetal transition-font dark:text-silver">Tax</p>
+                            </td>
+                            <!-- item tax -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.itemTax }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;item</p>
+                                </div>
+                            </td>
+                            <!-- split tax -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.splitTax }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;person</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- tip -->
+                        <tr class="border-b border-gunmetal border-opacity-25 transition dark:border-silver dark:border-opacity-25">
+                            <!-- label -->
+                            <td class="w-full px-4 py-2">
+                                <p class="font-bold text-gunmetal transition-font dark:text-silver">Tip</p>
+                            </td>
+                            <!-- item tip -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.itemTip }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;item</p>
+                                </div>
+                            </td>
+                            <!-- split tip -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.splitTip }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;person</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- total -->
+                        <tr>
+                            <!-- label -->
+                            <td class="w-full px-4 py-2">
+                                <p class="font-bold text-gunmetal transition-font dark:text-silver">Total</p>
+                            </td>
+                            <!-- item total -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-charcoal transition-font dark:text-ice">${{ item.amounts.itemTotal }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;item</p>
+                                </div>
+                            </td>
+                            <!-- split total -->
+                            <td class="w-full px-4 py-2">
+                                <div class="flex items-center justify-end gap-2">
+                                    <h1 class="text-xl font-bold text-blue">${{ item.amounts.splitTotal }}</h1>
+                                    <p class="whitespace-nowrap text-sm text-gunmetal transition-font dark:text-silver">/&ensp;person</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </div>
-    <!-- test -->
-    <div class="flex flex-col gap-4">
-        <span>{{ event }}</span>
-        <span>{{ party }}</span>
-        <span>{{ check }}</span>
-        <span>{{ food }}</span>
-        <span>{{ results }}</span>
     </div>
 </template>
 
