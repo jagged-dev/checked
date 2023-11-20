ab24cc3b41723975396c3d1c7919ed38aebdbe5d
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-11-20 12:20:00 -0800

    Updated Results.vue

--- src/views/wizard/tabs/Results.vue
+++ src/views/wizard/tabs/Results.vue
@@ -9,12 +9,10 @@ const props = defineProps({
 });
 
 const results = ref<any>({});
-const expanded = ref();
+const expanded = ref("results");
 
 onMounted(() => {
     if (props.event && props.party && props.check && props.food) {
-        expanded.value = props.party[0];
-
         let taxPct = Number(props.check.tax) / Number(props.check.subtotal);
         let tipPct = Number(props.check.tip) / Number(props.check.subtotal);
 
@@ -59,14 +57,14 @@ onMounted(() => {
     }
 });
 
-function toggleGuest(guest: string, selected: boolean) {
-    if (!selected) expanded.value = guest;
+function toggleResult(result: string, selected: boolean) {
+    if (!selected) expanded.value = result;
     else expanded.value = "";
 }
 </script>
 
 <template>
-    <!-- results -->
+    <!-- overview -->
     <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:p-12">
         <!-- heading -->
         <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ event }}</h1>
@@ -74,15 +72,48 @@ function toggleGuest(guest: string, selected: boolean) {
         <md-divider></md-divider>
         <!-- guests -->
         <div class="flex flex-wrap gap-2">
-            <md-assist-chip label="Results" disabled>
-                <md-icon slot="icon">filter_list</md-icon>
-            </md-assist-chip>
-            <md-filter-chip :label="guest" :selected="guest === expanded" @click="toggleGuest(guest, $event.target.selected)" v-for="guest in party">
+            <md-filter-chip label="Results" :selected="'results' === expanded" @click="toggleResult('results', $event.target.selected)">
+                <md-icon slot="icon">arrow_drop_down</md-icon>
+            </md-filter-chip>
+            <md-filter-chip :label="guest" :selected="guest === expanded" @click="toggleResult(guest, $event.target.selected)" v-for="guest in party">
                 <md-icon slot="icon">arrow_drop_down</md-icon>
             </md-filter-chip>
         </div>
     </div>
-    <!-- details -->
+    <!-- results -->
+    <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:p-12" :class="{ hidden: 'results' !== expanded }">
+        <!-- heading -->
+        <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">Results:&ensp;${{ check?.total }}</h1>
+        <!-- guests -->
+        <table>
+            <thead>
+                <tr>
+                    <th class="text-start font-bold text-charcoal transition-font dark:text-ice">Guest</th>
+                    <th class="text-end font-bold text-charcoal transition-font dark:text-ice">Subtotal</th>
+                    <th class="text-end font-bold text-charcoal transition-font dark:text-ice">Tax</th>
+                    <th class="text-end font-bold text-charcoal transition-font dark:text-ice">Tip</th>
+                    <th class="text-end font-bold text-charcoal transition-font dark:text-ice">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="transition-background hover:bg-gunmetal hover:bg-opacity-10 dark:hover:bg-silver dark:hover:bg-opacity-10" v-for="guest in party">
+                    <td class="text-start text-gunmetal transition-font dark:text-silver">{{ guest }}</td>
+                    <td class="text-end text-gunmetal transition-font dark:text-silver">${{ results[guest]?.subtotal }}</td>
+                    <td class="text-end text-gunmetal transition-font dark:text-silver">${{ results[guest]?.tax }}</td>
+                    <td class="text-end text-gunmetal transition-font dark:text-silver">${{ results[guest]?.tip }}</td>
+                    <td class="text-end text-gunmetal transition-font dark:text-silver">${{ results[guest]?.total }}</td>
+                </tr>
+                <tr class="transition-background hover:bg-gunmetal hover:bg-opacity-10 dark:hover:bg-silver dark:hover:bg-opacity-10">
+                    <td class="text-start text-charcoal transition-font dark:text-ice">TOTAL</td>
+                    <td class="text-end text-charcoal transition-font dark:text-ice">${{ check?.subtotal }}</td>
+                    <td class="text-end text-charcoal transition-font dark:text-ice">${{ check?.tax }}</td>
+                    <td class="text-end text-charcoal transition-font dark:text-ice">${{ check?.tip }}</td>
+                    <td class="text-end text-blue">${{ check?.total }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- guests -->
     <div class="flex flex-col gap-4 rounded-3xl bg-ice p-8 transition-background dark:bg-charcoal xl:p-12" :class="{ hidden: guest !== expanded }" v-for="guest in party">
         <!-- heading -->
         <h1 class="text-2xl font-bold text-charcoal transition-font dark:text-ice">{{ guest }}:&ensp;${{ results[guest]?.total }}</h1>
@@ -98,14 +129,14 @@ function toggleGuest(guest: string, selected: boolean) {
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="item in results[guest]?.items">
+                <tr class="transition-background hover:bg-gunmetal hover:bg-opacity-10 dark:hover:bg-silver dark:hover:bg-opacity-10" v-for="item in results[guest]?.items">
                     <td class="text-start text-gunmetal transition-font dark:text-silver">{{ item.name }}</td>
                     <td class="text-end text-gunmetal transition-font dark:text-silver">${{ item.subtotal }}</td>
                     <td class="text-end text-gunmetal transition-font dark:text-silver">${{ item.tax }}</td>
                     <td class="text-end text-gunmetal transition-font dark:text-silver">${{ item.tip }}</td>
                     <td class="text-end text-gunmetal transition-font dark:text-silver">${{ item.total }}</td>
                 </tr>
-                <tr>
+                <tr class="transition-background hover:bg-gunmetal hover:bg-opacity-10 dark:hover:bg-silver dark:hover:bg-opacity-10">
                     <td class="text-start text-charcoal transition-font dark:text-ice">TOTAL</td>
                     <td class="text-end text-charcoal transition-font dark:text-ice">${{ results[guest]?.subtotal }}</td>
                     <td class="text-end text-charcoal transition-font dark:text-ice">${{ results[guest]?.tax }}</td>
