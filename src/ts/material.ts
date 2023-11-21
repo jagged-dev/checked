6acd4e9619f81267afa11f48dc81e1a126e9342e
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-11-20 21:49:47 -0800

    Updated material.ts

--- src/ts/material.ts
+++ src/ts/material.ts
@@ -1,60 +1,32 @@
 // button
-import "@material/web/button/elevated-button";
 import "@material/web/button/filled-button";
 import "@material/web/button/filled-tonal-button";
-import "@material/web/button/outlined-button";
 import "@material/web/button/text-button";
-
-// checkbox
-import "@material/web/checkbox/checkbox";
 
 // chips
 import "@material/web/chips/assist-chip";
 import "@material/web/chips/filter-chip";
-import "@material/web/chips/input-chip";
 import "@material/web/chips/suggestion-chip";
 
 // divider
 import "@material/web/divider/divider";
-
-// elevation
-import "@material/web/elevation/elevation";
-
-// fab
-import "@material/web/fab/fab";
-import "@material/web/fab/branded-fab";
 
 // icon
 import "@material/web/icon/icon";
 
 // icon button
 import "@material/web/iconbutton/icon-button";
-import "@material/web/iconbutton/filled-icon-button";
-import "@material/web/iconbutton/filled-tonal-icon-button";
 import "@material/web/iconbutton/outlined-icon-button";
 
 // progress
 import "@material/web/progress/linear-progress";
 
-// radio
-import "@material/web/radio/radio";
-
 // ripple
 import "@material/web/ripple/ripple";
-
-// select
-import "@material/web/select/filled-select";
-import "@material/web/select/outlined-select";
-import "@material/web/select/select-option";
-
-// switch
-import "@material/web/switch/switch";
 
 // tabs
 import "@material/web/tabs/tabs";
 import "@material/web/tabs/primary-tab";
-import "@material/web/tabs/secondary-tab";
 
 // text field
 import "@material/web/textfield/filled-text-field";
-import "@material/web/textfield/outlined-text-field";
